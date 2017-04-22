5ac46b950ed15a77aaea9448ea29d320c35ebb14
Alistair Mann <al+tfl@pectw.net>
2017-04-22 01:34:08 +0100

    Add the mainLoop as a callback for geolocation.watchPosition()

--- server-code/js/scripts.js
+++ server-code/js/scripts.js
@@ -2,6 +2,30 @@
   eztfl-html5  Alistair Mann
 */
 
+//
+// setup helpers
+//
 function eztflHtml5_setup() {
-    alert('Hello, world!');
+    getLocationSetup()
+}
+
+//
+// geolocation helpers
+//
+function getLocationSetup() {
+    if (navigator.geolocation) {
+	navigator.geolocation.watchPosition(mainLoop);
+
+    } else {
+	alert('Geolocation is not supported by this browser.');
+    }
+}
+
+//-------------------------------------------------------------
+//
+// mainLoop
+//
+function mainLoop(position) {
+    alert('Latitude: ' + position.coords.latitude +
+	  'Longitude: ' + position.coords.longitude);
 }
