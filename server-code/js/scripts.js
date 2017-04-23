31aa5719ca6b1eae56f159cabaa4634cb00bf88a
Alistair Mann <al+tfl@pectw.net>
2017-04-23 11:24:53 +0100

    Intermediate commit that should really be in another branch
    Commit has unfinished and untested trigonometry routines for
    the prediction. Committing just to show got to this point
    before realising a couple more Unknown unknowns need
    resolving beforehand

--- server-code/js/scripts.js
+++ server-code/js/scripts.js
@@ -39,6 +39,69 @@ function getLocationSetup() {
     }
 }
 
+function getSpeed(position1, position2, distance) {
+    var seconds = position2.timestamp - position1.timestamp;
+    return seconds / distance;
+}
+
+//
+// trigonometry helpers
+//
+function se_deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
+function se_rad2deg(n) {
+    return n * (180 / Math.PI);
+}
+
+function getHeading(position1, position2) {
+    return se_getBearing(position1.coords.latitude,
+			 position1.coords.longitude,
+			 position2.coords.latitude,
+			 position2.coords.longitude);
+}
+
+function se_getBearing(startLat, startLong, endLat, endLong) {
+    startLat = se_deg2rad(startLat);
+    startLong = se_deg2rad(startLong);
+    endLat = se_deg2rad(endLat);
+    endLong = se_deg2rad(endLong);
+
+    var dLong = endLong - startLong;
+
+    var dPhi = Math.log(Math.tan(endLat / 2.0 + Math.PI / 4.0)
+			/ Math.tan(startLat / 2.0 + Math.PI / 4.0));
+    if (Math.abs(dLong) > Math.PI){
+	if (dLong > 0.0)
+	    dLong = -(2.0 * Math.PI - dLong);
+	else
+	    dLong = (2.0 * Math.PI + dLong);
+    }
+
+    return (se_rad2deg(Math.atan2(dLong, dPhi)) + 360.0) % 360.0;
+}
+
+function getDistance(position1, position2) {
+    return se_getDistanceFromLatLonInKm(position1.coords.latitude,
+					position1.coords.longitude,
+					position2.coords.latitude,
+					position2.coords.longitude);
+}
+
+function se_getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+	Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+	Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+	Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
 //-------------------------------------------------------------
 //
 // other helpers
@@ -63,7 +126,30 @@ function coordsPush(num_pairs, position) {
 // prediction helpers
 //
 function coordsGetPrediction(num_coords_tracked) {
-    return ['prediction ' + num_coords_tracked];
+    return coordsGetPredictionSimplest(num_coords_tracked);
+}
+
+function coordsGetPredictionSimplest(num_coords_tracked) {
+    // take first and last coord in stack, ignore rest
+    var early_coord = [];
+    var latest_coord = [];
+    var distance;
+    var heading;
+    var speed;
+
+    if (num_coords_tracked < 2)
+	return ['Waiting for more data'];
+
+    early_coord = tracked_coord_pairs[NUM_TRACKED_COORD_PAIRS
+				      -  num_coords_tracked - 1];
+    latest_coord = tracked_coord_pairs[NUM_TRACKED_COORD_PAIRS - 1];
+
+    distance = getDistance(early_coord, latest_coord);
+    heading = getHeading(early_coord, latest_coord);
+    speed = getSpeed(early_coord, latest_coord, distance);
+
+    return ['prediction: d=' + distance + ' h=' + heading
+	   + ' s=' + speed];
 }
 
 //
